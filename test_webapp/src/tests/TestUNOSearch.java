baa49f48848cd744332ed9077fe63ca788d23cbd
MoistyRain <111257933+MoistyRain@users.noreply.github.com>
2024-04-30 19:56:49 -0500

    initial commit

---
+++ test_webapp/src/tests/TestUNOSearch.java
@@ -0,0 +1,82 @@
+package tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.time.Duration;
+
+public class TestUNOSearch {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\marvi\\CSCI4830\\workspace\\workspace4830-webapp-testing-Orellana\\project_webapp_testing1\\lib\\win\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testUNOSearch() throws Exception {
+	driver.manage().window().maximize();
+    driver.get("https://www.unomaha.edu/");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Search UNO'])[1]/following::*[name()='svg'][1]")).click();
+    driver.findElement(By.id("search-input")).click();
+    driver.findElement(By.id("search-input")).clear();
+    driver.findElement(By.id("search-input")).sendKeys("Computer Science");
+    driver.findElement(By.id("search-form")).submit();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
